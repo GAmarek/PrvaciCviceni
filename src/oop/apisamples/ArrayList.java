c9af32ad39a411dd12684ec0076ff7b3935886da
moos <moos@P1-KATEDRA.p1.gyarab.cz>
2016-02-09 11:25:29 +0100

    init version

---
+++ src/oop/apisamples/ArrayList.java
@@ -0,0 +1,28 @@
+package oop.apisamples;
+
+public class ArrayList {
+    private int[] pole;
+    
+    public ArrayList(){
+    }
+    
+    public ArrayList(int[] pole){
+    }
+    
+    public ArrayList(ArrayList list){
+    }
+    
+    public int get(int index){
+    }    
+    
+    public void add(int prvek){
+    }    
+    
+    public void sort(){
+    }
+    
+    public int size(){
+        
+    }
+    
+}
---
+++ src/oop/apisamples/ExerciseScanner.java
@@ -0,0 +1,61 @@
+package oop.apisamples;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ExerciseScanner {
+
+    public static void nactiVstup(Scanner scanner, ArrayList list) {
+        while (scanner.hasNext()) {
+            list.add(scanner.nextInt());
+        }
+    }
+
+public static void seradList(ArrayList list) {
+        list.sort(null);
+    }
+
+    public static void vypisList(ArrayList list) {
+        for (int index = 0; index < list.size(); index++) {
+            System.out.println(list.get(index));
+        }
+    }
+
+    public static void zpracujVstup(Scanner scanner) {
+        ArrayList list = new ArrayList();
+
+        nactiVstup(scanner, list);
+        System.out.println();
+        seradList(list);
+        vypisList(list);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner;
+        
+        if(args.length == 0){
+            System.out.println("Pouzijte argument pro typ vstupu. Podporovane hodnoty: console|soubor|retezec");            
+            return;
+        }
+        
+        switch(args[0]){
+            case "console":
+                scanner = new Scanner(System.in);
+                zpracujVstup(scanner);
+                break;
+            case "soubor":
+                File file = new File("test.txt");
+                scanner = new Scanner(file);
+                zpracujVstup(scanner);
+                break;
+            case "retezec":
+                String testString = "8 4 2 1 0";
+                scanner = new Scanner(testString);
+                zpracujVstup(scanner);
+                break;
+            default:
+                System.out.println("Tento typ vstupu neni podporovan");
+        }        
+    }
+}
